4fffde6f9cd05775106a22d79f3646e0354619eb
João gabriel <86538352+Blu-guy@users.noreply.github.com>
2025-05-13 21:48:43 -0300

    Add files via upload

---
+++ ed_02/src/ContaSalario.java
@@ -0,0 +1,21 @@
+//subclasse conta salario
+
+public class ContaSalario extends Conta {
+
+    public ContaSalario(String cliente, double saldo) {
+        super(cliente, saldo);
+    }
+
+    @Override
+    public void aplicarJurosDiarios(){
+        // contas salarios não tem juros
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("Extrato da Conta Salário: ");
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Saldo: R$" + saldo);
+        System.out.println("-----------------------------------------");
+    }
+}
