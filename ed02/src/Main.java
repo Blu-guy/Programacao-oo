179cacba47220233178497a0743d8f8ef7be0b40
João gabriel <86538352+Blu-guy@users.noreply.github.com>
2025-05-16 21:08:03 -0300

    Update Main.java

--- ed02/src/Main.java
+++ ed02/src/Main.java
@@ -1,14 +1,14 @@
-public class Main {
+public class Main { //main fornecido pelo professor
     public static void main(String[] args) {
-        ContaCorrente cc = new ContaCorrente("Alice", 1000);
+        ContaCorrente cc = new ContaCorrente("Alice", 1000);//os tipos de conta diferente com dados
         ContaPoupanca cp = new ContaPoupanca("Bob", 1500);
         ContaSalario cs = new ContaSalario("Carlos", 1200);
 
-        cc.depositar(200);
+        cc.depositar(200);//acoes que as contas vao realizar
         cp.sacar(100);
         cs.transferir(cc, 300);
 
-        cc.aplicarJurosDiarios();
+        cc.aplicarJurosDiarios();// aplicacao de jurosde cada classe
         cp.aplicarJurosDiarios();
         cs.aplicarJurosDiarios();
 
